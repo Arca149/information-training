102e4348de2f08ac3e71f20a943c752408432d4d
Arca <144094007+SynapseArca@users.noreply.github.com>
2024-09-24 03:57:10 +0000

    2024-9/24 ToDoリストの追加

--- 9-24/main.js
+++ 9-24/main.js
@@ -1,18 +1,33 @@
-const todoList = document.getElementById('todoList');
-window.addTodo = function(){
-    const todoInput = document.getElementById('todoInput');
-    const todoText = todoInput.value.trim();
-    
-    if (todoText !== ""){
-        addTodoToList(todoText);
-        todoInput.value = "";
+document.addEventListener('DOMContentLoaded', function(){
+    const todoList = document.getElementById('todoList');
+    loadTodos();
+    function saveTodoLocalStrage(todoText){
+        let todos = JSON.parse(localStorage.getItem('todos')) || [];
+        todos.push(todoText);
+        localStorage.setItem('todos', JSON.stringify(todos));
     }
-    else{
-        alert("やることを入力してください");
+    function loadTodos(){
+        let todos = JSON.parse(localStorage.getItem('todos')) || [];
+        todos.forEach(function(todo){
+            addTodoToList(todo);
+        });
     }
-}
-function addTodoToList(todoText) {
-    const li = document.createElement('li');
-    li.textContent = todoText;
-    todoList.appendChild(li);
-}
+    window.addTodo = function(){
+        const todoInput = document.getElementById('todoInput');
+        const todoText = todoInput.value.trim();
+        
+        if (todoText !== ""){
+            addTodoToList(todoText);
+            saveTodoLocalStrage(todoText);
+            todoInput.value = "";
+        }
+        else{
+            alert("やることを入力してください");
+        }
+    }
+    function addTodoToList(todoText) {
+        const li = document.createElement('li');
+        li.textContent = todoText;
+        todoList.appendChild(li);
+    }
+});
